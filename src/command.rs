77b06871c59c1e838d0bdb1339f80ae3cf8fc613
Your Name <you@example.com>
2023-11-23 16:25:41 +0800

    trait super test

--- src/command.rs
+++ src/command.rs
@@ -1,6 +1,10 @@
 use std::io::ErrorKind;
 use std::process::Termination;
 use std::process::ExitCode;
+use add_two::Iadd;
+use add_two::Iadd2;
+use add_two::Isub;
+use add_two::IMath;
 #[derive(Debug)]
 pub enum Error {
     Io( std::io::Error),
@@ -16,3 +20,35 @@ pub fn run() -> Result<String,Error> {
     Err(Error::Io(std::io::Error::new(ErrorKind::AddrInUse,"")))
 }
 
+pub struct Execor{
+    pub j:i32
+}
+impl  Iadd for Execor{
+    fn adds(&self,mut i: i32) -> i32{
+        i=i+self.j+1;
+        i
+    }
+
+}
+
+impl  Iadd2 for Execor{
+    fn adds2(&self,mut i: i32) -> i32{
+        i=i+self.j+2;
+        i
+   }
+}
+
+
+
+
+impl  Isub for Execor{
+
+    fn subs(&self,mut i: i32) -> i32{
+        i=self.j+i-1;
+        i
+    }
+
+}
+impl IMath for Execor{
+
+}
